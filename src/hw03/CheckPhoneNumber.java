8cfc705895c4274abb568a98593f25689319b988
lex0819 <lexmolnar@gmail.com>
2023-08-06 12:40:36 +0300

    10 or 11 digits for phone number

--- src/hw03/CheckPhoneNumber.java
+++ src/hw03/CheckPhoneNumber.java
@@ -2,6 +2,6 @@ package hw03;
 
 public class CheckPhoneNumber extends RuntimeException{
     public CheckPhoneNumber(){
-        super("Phone number must have only 11 digits!");
+        super("Phone number must have only 10 or 11 digits!");
     }
 }
